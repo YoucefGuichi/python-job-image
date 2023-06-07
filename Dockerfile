45688a0a126400ccbe075cef6c31ce584639e44e
Youcef Guichi <youcef@gimlet.io>
2023-06-07 11:18:25 +0200

    Create rabbitmq consumer

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 from python:latest
 
+RUN pip install pika
+
 copy main.py main.py
 
 cmd ["python3", "main.py"]
--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-import time
-if __name__ == "__main__":
-    for i in range(10):
-        print("sleep for 0.5 second ...")
-        time.sleep(0.5)
-        print(i)
+import pika, sys, os
+
+def main():
+    credentials = pika.PlainCredentials("guest","guest")
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost',port=8080, credentials=credentials))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='publisher')
+
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % body)
+
+    channel.basic_consume(queue='publisher', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
       
