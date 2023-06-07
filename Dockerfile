7b7d0a1ff9e649f0726ac0c157da91d1670c7845
Youcef Guichi <youcef@gimlet.io>
2023-06-07 12:43:20 +0200

    Enhancements -- Create consumer pod manifest

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,3 @@ from python:latest
 RUN pip install pika
 
 copy main.py main.py
-
-cmd ["python3", "main.py"]
--- main.py
+++ main.py
@@ -2,24 +2,24 @@ import pika, sys, os
 
 def main():
     credentials = pika.PlainCredentials("guest","guest")
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost',port=8080, credentials=credentials))
-    channel = connection.channel()
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq',port=5672, credentials=credentials))
+    channel = connection.channel(channel_number=1)
 
-    channel.queue_declare(queue='publisher')
+    # channel.queue_declare(queue='publisher')
 
     def callback(ch, method, properties, body):
-        print(" [x] Received %r" % body)
+        print(" [x] Received %r" % body, flush=True)
 
     channel.basic_consume(queue='publisher', on_message_callback=callback, auto_ack=True)
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
+    print(' [*] Waiting for messages. To exit press CTRL+C',  flush=True)
     channel.start_consuming()
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print('Interrupted')
+        print('Interrupted', flush=True)
         try:
             sys.exit(0)
         except SystemExit:
